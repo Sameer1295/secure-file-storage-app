6a2b4f192d461e571188902328a82c0dbe3a615c
Sameer Singh <sameersingh@osiansoftware.com>
2023-02-12 16:58:02 +0530

    template home page remove

--- secure_file_storage_app/urls.py
+++ secure_file_storage_app/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path('admin/', admin.site.urls),
     path("customuser/", include("customuser.urls")),
     path("customuser/", include("django.contrib.auth.urls")),
