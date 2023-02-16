dcc976aa451c6de51c3a3924287a24a3a76c35b5
Sameer Singh <sameersingh@osiansoftware.com>
2023-02-16 22:35:23 +0530

    version of fileupload with aes

--- file_storage/admin.py
+++ file_storage/admin.py
@@ -1,3 +1,4 @@
+import os
 from django.contrib import admin
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -10,38 +11,48 @@ from cryptography.fernet import Fernet
 from io import BytesIO
 import uuid
 from file_storage.models import FileStorage
+from secure_file_storage_app import settings
+import os
+from django.conf import settings
+from cryptography.fernet import Fernet
 
+def encrypt_file(file_path, key):
+    with open(file_path, 'rb') as f:
+        data = f.read()
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(data)
+    with open(file_path, 'wb') as f:
+        f.write(encrypted_data)
 
+# savemodel function works
+# need to add code to encrypt file using aes key
+    # first need access to file in request
+    # then that encrypted_filepath file is read and ecnrpyted file is generated
 class FileStorageAdmin(admin.ModelAdmin):
     exclude = ('filename','encrypted_aeskey','ecc_public_key',)
     
-    def upload_file(self, request):
-        # Read the uploaded file
-        file = request.FILES['file']
-        file_data = file.read()
-
+    def save_model(self, request, obj, form, change):
+        print("called........../////////////")
+        # Call the parent save_model method to save the model instance
+        super().save_model(request, obj, form, change)
+        uploaded_file = request.FILES['encrypted_filepath']
+        
         # Generate a new AES key
-        aes_key = Fernet.generate_key()
+        key = Fernet.generate_key()
+        # Encrypt the file and save it to disk
+        file_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
+        with open(file_path, 'wb') as f:
+            f.write(uploaded_file.read())
+        encrypt_file(file_path, key)
+        # Save the key and file path to the database
+        obj.aes_key = key
+        obj.encrypted_file = file_path
+        # Call the parent save_model method
+        super().save_model(request, obj, form, change)
+        
+        # encrypted_file_path = 'sdfsdf1111'+image_path.name
+        # key = 'sdfsdfdsffdsgdsgdsgsdg'
+        # FileStorage.objects.create(encrypted_filepath=encrypted_file_path, encrypted_aeskey=key)
 
-        # Encrypt the file data with the AES key
-        cipher = Fernet(aes_key)
-        encrypted_file_data = cipher.encrypt(file_data)
-
-        # Get the public key of the user who uploaded the file
-        user = User.objects.get(username=request.user.username)
-        public_key = RSA.import_key(user.public_key)
-
-        # Encrypt the AES key with the user's public key
-        cipher = RSA.new(public_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256), algorithm=hashes.SHA256, label=None))
-        encrypted_aes_key = cipher.encrypt(aes_key)
-
-        # Save the encrypted file data and encrypted AES key to the database
-        file_obj = File.objects.create(file_name=file.name, file_data=encrypted_file_data, encrypted_aes_key=encrypted_aes_key)
-        file_obj.save()
-
-        # Return a success response
-        response = HttpResponse(content_type='text/plain')
-        response.write('File uploaded successfully')
-        return response
-# Register your models here.
+# # Register your models here.
 admin.site.register(FileStorage, FileStorageAdmin)
