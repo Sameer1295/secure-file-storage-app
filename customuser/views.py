ed4984bef557317e1fe04fc45cb5b054ac57b1e6
Sameer Singh <sameersingh@osiansoftware.com>
2023-03-13 00:19:19 +0530

    file download based on created_by users rsa key and rsa key generation instead of ecc in user model

--- customuser/views.py
+++ customuser/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 from .forms import CustomUserCreationForm
 
@@ -12,23 +12,24 @@ class SignUpView(CreateView):
     template_name = "registration/signup.html"
     
 def generate_ecc_key_pair(user):
-    # Generate the ECC key pair
-    private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
+    # Generate an RSA key pair
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
     public_key = private_key.public_key()
-
+    # Serialize the public key
+    public_key_serialized = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    
     # Serialize the private key
     private_key_serialized = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
+        encryption_algorithm=serialization.NoEncryption(),
     )
-
-    # Serialize the public key
-    public_key_serialized = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-
     # Store the serialized keys in the database
     user.public_key = public_key_serialized.decode('utf-8')
     user.private_key = private_key_serialized.decode('utf-8')
--- file_storage/adminViews.py
+++ file_storage/adminViews.py
@@ -4,6 +4,7 @@
 import base64
 import os
 from django.shortcuts import render
+from customuser.models import CustomUser
 
 from file_storage.models import FileStorage
 from secure_file_storage_app import settings
@@ -26,10 +27,10 @@ class DownloadView(View):
         file = FileStorage.objects.get(id=file_storage_id)
         encrypted_path = file.filename
         aes_key = file.encrypted_aeskey
-
+        created_by_user = CustomUser.objects.get(pk=file.created_by)
         # Decrypt the encrypted AES key using the user's RSA private key
         private_key = serialization.load_pem_private_key(
-            request.user.private_key.encode('utf-8'),
+            created_by_user.private_key.encode('utf-8'),
             password=None,
             backend=default_backend(),
         )
